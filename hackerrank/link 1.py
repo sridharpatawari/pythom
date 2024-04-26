7cc42e2eadc124eea2c1ab13319855d438136bc3
Sridhar patawari <160710354+sridharpatawari@users.noreply.github.com>
2024-04-26 22:44:09 +0530

    Add files via upload

---
+++ hackerrank/link 1.py
@@ -0,0 +1,28 @@
+#link 1:
+'''
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 != 0:
+        print("Weird")
+    elif n in range(2, 6):
+        print("Not Weird")
+    elif n in range(6, 21):
+        print("Weird")
+    elif n > 20:
+        print("Not Weird")
+'''
+'''
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
+'''
+'''
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+'''
